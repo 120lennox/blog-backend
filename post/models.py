322bd662a35eaa142ed20cb36be0272020d5e414
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-23 15:34:50 +0200

    added a database for the blog app

--- post/models.py
+++ post/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Post(models.Model):
-    pass
+    CATEGORIES = [
+        ('Leadership', 'Leadership'),
+        ('Motivation', 'Motivation'),
+        ('Inspiration', 'Inspiration'),
+        ('Technology', 'Technology'),
+        ('Frameworks', 'Frameworks'),
+        ('Design', 'Design'),
+        ('Research', 'Research'),
+        ('Software Development', 'Software Development'),
+    ]
+
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category = models.CharField(
+        max_length=200,
+        choices=CATEGORIES,
+        blank=True,
+        help_text='Select category'
+    )
+
+    def __str__(self):
+        return self.title  
