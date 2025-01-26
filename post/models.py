072ae5bd02ee0554997b949c9ba20d8760119025
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-26 13:23:17 +0200

    modifying backend here and there

--- post/models.py
+++ post/models.py
@@ -3,23 +3,33 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    PREDEFINED_CATEGORIES = [
+        ('Leadership', 'Leadership'),
+        ('Motivation', 'Motivation'),
+        ('Inspiration', 'Inspiration'),
+        ('Technology', 'Technology'),
+        ('Frameworks', 'Frameworks'),
+        ('Design', 'Design'),
+        ('Research', 'Research'),
+        ('Software Development', 'Software Development'),]
+    
+    name = models.CharField(max_length=200, choices=PREDEFINED_CATEGORIES, unique=True)
 
     def __str__(self):
         return self.name
 class Post(models.Model):
-    PREDEFINED_CATEGORIES = [
-        # ('Leadership', 'Leadership'),
-        # ('Motivation', 'Motivation'),
-        # ('Inspiration', 'Inspiration'),
-        # ('Technology', 'Technology'),
-        # ('Frameworks', 'Frameworks'),
-        # ('Design', 'Design'),
-        # ('Research', 'Research'),
-        # ('Software Development', 'Software Development'),
-        'Leadership', 'Motivation', 'Inspiration', 'Technology', 
-        'Frameworks', 'Design', 'Research', 'Software Development'
-    ]
+    # PREDEFINED_CATEGORIES = [
+    #     # ('Leadership', 'Leadership'),
+    #     # ('Motivation', 'Motivation'),
+    #     # ('Inspiration', 'Inspiration'),
+    #     # ('Technology', 'Technology'),
+    #     # ('Frameworks', 'Frameworks'),
+    #     # ('Design', 'Design'),
+    #     # ('Research', 'Research'),
+    #     # ('Software Development', 'Software Development'),
+    #     'Leadership', 'Motivation', 'Inspiration', 'Technology', 
+    #     'Frameworks', 'Design', 'Research', 'Software Development'
+    # ]
 
     title = models.CharField(max_length=100)
     body = models.TextField()
@@ -29,10 +39,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title  
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        # automatically create predefined categories if they dont have any
-        for category in self.PREDEFINED_CATEGORIES:
-            Category.objects.get_or_create(name=category)
+    #     # automatically create predefined categories if they dont have any
+    #     for category in self.PREDEFINED_CATEGORIES:
+    #         Category.objects.get_or_create(name=category)
 
