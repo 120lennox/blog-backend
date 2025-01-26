7ac1a12b72e703f9906db1d8eadcec654a73e2c9
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-26 14:17:09 +0200

    added admin presentation of categories

--- post/admin.py
+++ post/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from .models import Post, Category
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_categories')
+    filter_horizontal = ('categories',)
+
+    def display_categories(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    display_categories.short_description = 'Categories'
 
 
