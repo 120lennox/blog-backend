ee5b70085e6ec6d25e77b43ab66c4e66aacb6155
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-26 13:05:27 +0200

    made changes in db schema

--- post/models.py
+++ post/models.py
@@ -1,27 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
 class Post(models.Model):
-    CATEGORIES = [
-        ('Leadership', 'Leadership'),
-        ('Motivation', 'Motivation'),
-        ('Inspiration', 'Inspiration'),
-        ('Technology', 'Technology'),
-        ('Frameworks', 'Frameworks'),
-        ('Design', 'Design'),
-        ('Research', 'Research'),
-        ('Software Development', 'Software Development'),
+    PREDEFINED_CATEGORIES = [
+        # ('Leadership', 'Leadership'),
+        # ('Motivation', 'Motivation'),
+        # ('Inspiration', 'Inspiration'),
+        # ('Technology', 'Technology'),
+        # ('Frameworks', 'Frameworks'),
+        # ('Design', 'Design'),
+        # ('Research', 'Research'),
+        # ('Software Development', 'Software Development'),
+        'Leadership', 'Motivation', 'Inspiration', 'Technology', 
+        'Frameworks', 'Design', 'Research', 'Software Development'
     ]
 
     title = models.CharField(max_length=100)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    category = models.CharField(
-        max_length=200,
-        choices=CATEGORIES,
-        blank=True,
-        help_text='Select category'
-    )
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.title  
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # automatically create predefined categories if they dont have any
+        for category in self.PREDEFINED_CATEGORIES:
+            Category.objects.get_or_create(name=category)
+
